c11dd47f852677c083920ca508fe29024d21df2c
rahul <rahulbittuverma26@gmail.com>
2024-03-04 22:08:11 +0530

    Final Commit

---
+++ Vehicle_Configurator_dotNet/vehicle_configurator_MS.Net/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace vehicle_configurator.Models;
+
+public partial class User
+{
+    [Required(ErrorMessage = "Password is required")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Password correction")]
+    [Column("password")]
+    public string? Password { get; set; }
+    public string? Username { get; set; }
+}
